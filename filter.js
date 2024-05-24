91161f529771f0c1fa8a7aca391dfb0d925af7a3
uday kumar <kumaruday628@gmail.com>
2024-05-23 18:32:59 -1200

    filter changes

--- filter.js
+++ filter.js
@@ -2,12 +2,20 @@ const players=['sachin','ganguly',"dravid",'yuvi','panth','zaheer','gambir','boo
 
 const playerRef=document.querySelector('#list');
 
-function prepareList(){
-    const playerstr=players.map((val)=>{
+function prepareList(data){
+    const playerstr=data.map((val)=>{
         return `<li>${val}</li>`
     }).join('');
     const list=`<ul>${playerstr}</ul>`;
     playerRef.innerHTML=list;
+    
+    }
+    function handleChange(eve){
+        const value=eve.target.value;
+        const filterplayers=players.filter((val)=>{
+            return val.toLocaleLowerCase().startsWith(value.toLocaleLowerCase());
+        })
+        prepareList(filterplayers);
 
 }
-prepareList();
+prepareList(players);
